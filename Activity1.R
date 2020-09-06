68a12c9a2d57016238f6f6f14891fa87500eb1f2
MattMcGraw <mmcgraw@colgate.edu>
2020-09-06 13:54:48 -0400

    Added vector equations and calculations to code

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,14 @@
 print("Hello World")
+
+#name a certain number a
+a <- 48
+
+#name a solution b
+b <- a * 50 / 30 + 10 
+
+#use both variables in new equation
+a + b -38 + 1
+
+#name another solution using past variables
+c <- b - a / 2 + 100
+c
