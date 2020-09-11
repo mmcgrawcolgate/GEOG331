4c78c0bd74229e64822a6aacbec5dc2679ff631c
MattMcGraw <mmcgraw@colgate.edu>
2020-09-11 11:22:20 -0400

    added and organized code up through mean

--- Activity2.R
+++ Activity2.R
@@ -1,3 +1,19 @@
-datW <- read.csv("noaa_weather\\2011124.csv")
-getwd()
+#ACTIVITY 2 CODE
+
+#reads data into variable datW
+datW <- read.csv("noaa_weather//2011124.csv")
+
+#displays overall stats and figures from data
 str(datW)
+
+#formatting date column
+datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
+datW$year <- as.numeric(format(datW$dateF,"%Y"))
+
+#question 2 code
+
+#check unique site names
+levels(datW$NAME)
+
+#find mean of aberdeen
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm=TRUE)
