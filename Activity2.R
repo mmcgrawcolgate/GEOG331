454bb508485d1e9e6131c2e134ba6a49498a6b6a
MattMcGraw <mmcgraw@colgate.edu>
2020-09-16 21:29:12 -0400

    Code up through initial histogram

--- Activity2.R
+++ Activity2.R
@@ -17,3 +17,26 @@ levels(datW$NAME)
 
 #find mean of aberdeen
 mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm=TRUE)
+
+#find average daily temperature
+datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
+
+#find mean not only of aberdeen but across all sites
+averageTemp <- aggregate(datW$TAVE, by=list(datW$NAME), FUN="mean",na.rm=TRUE)
+averageTemp
+
+#change column names so they display name and mean annual air temp
+colnames(averageTemp) <- c("NAME","MAAT")
+averageTemp
+
+#convert level to numbers
+datW$siteN <- as.numeric(datW$NAME)
+
+#create histogram
+hist(datW$TAVE[datW$siteN == 1],
+     freq=FALSE, 
+     main = paste(levels(datW$NAME)[1]),
+     xlab = "Average daily temperature (degrees C)", 
+     ylab="Relative frequency",
+     col="grey50",
+     border="white")
