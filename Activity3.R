e5648a9771a4af0fefc4c087f166debab26ce4ad
MattMcGraw <mmcgraw@colgate.edu>
2020-09-28 15:18:26 -0400

    Updated code to Question 6

--- Activity3.R
+++ Activity3.R
@@ -33,7 +33,7 @@ print(sensorInfo)
 colnames(datW) <- colnames(sensorInfo)
 print(datW[1,])
 
-###QUESTION 4 (QA/QC)
+###DATA QA QC
 
 #convert to standardized formatting
 dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
@@ -62,3 +62,40 @@ length(which(is.na(datW$soil.temp)))
 #plot to see where data is missing
 plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
      ylab="Soil moisture (cm3 water per cm3 soil)")
+
+###QA QC TESTS
+
+#visual plot of air temperature
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+
+###QUESTION 4 CODE
+
+#new column to check for faulty data, freezing temps in the summer
+datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
+
+#checks quantiles for extreme data in the dataset
+quantile(datW$air.tempQ1)
+
+#finds days with very low air temperature
+datW[datW$air.tempQ1 < 8,] 
+
+#finds days with very high air temperature
+datW[datW$air.tempQ1 > 33,] 
+
+###QUESTION 5 CODE
+
+#plot precipitation and lightning strikes on the same plot
+lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
+
+#make the plot with precipitation and lightning activity marked
+plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
+     type="n")
+
+#plot precipitation points only when there is precipitation 
+points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0],
+       col= rgb(95/255,158/255,160/255,.5), pch=15)        
+
+#plot lightning points only when there is lightning     
+points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
+       col= "tomato3", pch=19)
